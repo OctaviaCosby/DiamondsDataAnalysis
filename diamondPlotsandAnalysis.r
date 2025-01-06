d04ca47be92e93c69e4a601212772d8d0acc446a
Octavia <ocosby2024@fau.edu>
2025-01-06 16:01:11 -0500

    import the necessary libraries

---
+++ diamondPlotsandAnalysis.r
@@ -0,0 +1,5 @@
+#import packages
+library("tidyverse") #load the tidyverse package
+diamonds #check to see if diamond dataframe is there
+diamond_data = diamonds #reassign data name
+diamond_data #print new variable to data
