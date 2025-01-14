fb48078fb0ac0df8c23ebf76c09cec622e183159
Octavia <ocosby2024@fau.edu>
2025-01-14 16:46:30 -0500

    scatter plot for clarity vs price

--- diamondPlotsandAnalysis.r
+++ diamondPlotsandAnalysis.r
@@ -3,3 +3,22 @@ library("tidyverse") #load the tidyverse package
 diamonds #check to see if diamond dataframe is there
 diamond_data = diamonds #reassign data name
 diamond_data #print new variable to data
+
+#
+#plot the data
+diamond_data_plot = diamond_data |> 
+  #to ensure data accuracy row with missing variables are removed
+  drop_na() |> 
+  #create a plot that assigns x, y axis and clarity using geom point
+  ggplot(mapping = aes(x = carat, y = price, color = clarity, background="white")) + 
+  #choose the type of graph
+  geom_point() + 
+  geom_smooth(method ="lm", se = FALSE) + geom_jitter() +
+  labs(title = "Correlation Between Carat Size, Clarity, and Price", y = "Price ($)", x= "Carat (weight)", color = "Clarity") +
+  theme(panel.background = element_rect(fill = "white"), panel.grid.major = element_line(color = "gray"))
+
+
+#saving the file with the width to match the height
+ggsave(filename = "diamond_plot_final.png", plot = diamond_data_plot, width= 7, height = 7)
+
+diamond_data_plot
